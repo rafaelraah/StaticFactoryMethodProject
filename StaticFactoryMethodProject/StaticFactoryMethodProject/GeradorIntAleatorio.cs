8dac1c83a0255bd9083d5f7877134028f96c471b
Rafael <rafaelsantos.rsdn@gmail.com>
2018-07-30 21:44:18 -0300

    Primeiro Commit

---
+++ StaticFactoryMethodProject/StaticFactoryMethodProject/GeradorIntAleatorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticFactoryMethodProject
+{
+    public class GeradorIntAleatorio
+    {
+        int min;
+        int max;
+        private GeradorIntAleatorio(int min, int max)
+        {
+            this.min = Math.Min(min, max);
+            this.max = Math.Max(min, max);
+        }
+        public static GeradorIntAleatorio getInstanceEntre(int min, int max)
+        {
+            return new GeradorIntAleatorio(min, max);
+        }
+    }
+}
